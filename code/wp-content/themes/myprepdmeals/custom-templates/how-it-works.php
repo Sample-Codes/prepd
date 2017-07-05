6dbde1d879ca0d3eb7e6ea40f314f89b192ea720
Cameron Cline <cam.cline7@gmail.com>
2017-07-05 15:41:03 -0700

    adding custom fields to how it works

--- code/wp-content/themes/myprepdmeals/custom-templates/how-it-works.php
+++ code/wp-content/themes/myprepdmeals/custom-templates/how-it-works.php
@@ -3,79 +3,78 @@
  * Template Name: How It Works
  *
  */
-get_header('banner'); ?>
+get_header('banner');
 
-    <!-- =============== How it Work image section =============== -->
-    <div class="work-section">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-12 wow bounceInLeft">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/pick-your-plan.jpg" alt="">
-          </div>
-        </div>
-      </div>
-    </div>
+$how_it_works_image = get_field('how_it_works_image');
+$section_one = get_field('content_section_one');
+$section_two_image = get_field('section_two_image');
+$section_two = get_field('content_section_two');
+$background_image = get_field('section_three_background_image');
+$section_three = get_field('content_section_three');
 
-    <!-- =============== Our Philosophy =============== -->
-    <div class="pick-plan parallax-2">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-12 wow fadeIn">
-            <h2 class="text-title-2">Pick Your Plan</h2>
-            <p>Our weekly plans make it easy to get the food you need each week as your busy schedule changes. Simply <a class="bl-link" href="http://www.myprepdmeals.com/meal-plans/">choose the plan</a> that makes sense for you: decide which portion size you crave, and how many meals per day you need. All our plans come with 2 hearty <a class="bl-link" href="http://www.myprepdmeals.com/between-meals-why-snacking-important/">snacks</a> per day to help keep you focused and on-track with your health goals. Your order will automatically renew each week so you don’t have to think twice! But don’t worry, we’re flexible – you can pause and renew your weekly orders at any time.</p>
-          </div>
-          <div class="col-sm-12">
-            <div class="text-xs-center" id="example-caption-1">Standard Plan</div>
-            <div class="progress">
-              <div class="progress-bar progress-bar-custom" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
-              </div>
-            </div>
-            <div class="text-xs-center" id="example-caption-2">Fuel Plan</div>
-            <div class="progress">
-              <div class="progress-bar progress-bar-full" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
-              </div>
-            </div>
+  // how it works image
+  echo '<div class="work-section">';
+  echo '<div class="container">';
+  echo '<div class="row">';
+  echo '<div class="col-sm-12 wow bounceInLeft">';
+  echo '<img src="' . $how_it_works_image . '" alt="how it works">';
+  echo '</div>';
+  echo '</div>';
+  echo '</div>';
+  echo '</div>';
 
-          </div>
-          <div class="col-sm-12 middle-img">
-            <a href="<?php echo site_url(); ?>/meal-plans/" class="btn btn-plan">Pick Your Plan</a>
-          </div>
-        </div>
-      </div>
-    </div>
+  // content section one
+  echo '<div class="pick-plan parallax-2">';
+  echo '<div class="container">';
+  echo '<div class="row">';
+  echo '<div class="col-sm-12 wow fadeIn">';
+  echo $section_one;
+  echo '</div>';
+  echo '<div class="col-sm-12">';
+  echo '<div class="text-xs-center" id="example-caption-1">Standard Plan</div>';
+  echo '<div class="progress">';
+  echo '<div class="progress-bar progress-bar-custom" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">';
+  echo '</div>';
+  echo '</div>';
+  echo '<div class="text-xs-center" id="example-caption-2">Fuel Plan</div>';
+  echo '<div class="progress">';
+  echo '<div class="progress-bar progress-bar-full" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">';
+  echo '</div>';
+  echo '</div>';
+  echo '</div>';
+  echo '<div class="col-sm-12 middle-img">';
+  echo '<a href="' . site_url() . '/meal-plans/" class="btn btn-plan">Pick Your Plan</a>';
+  echo '</div>';
+  echo '</div>';
+  echo '</div>';
+  echo '</div>';
+    
+  //content section two
+  echo '<div class="pick-meals">';
+  echo '<div class="container">';
+  echo '<div class="row">';
+  echo '<div class="col-sm-5">';
+  echo '<img src="' . $section_two_image . '" alt="" width="768" height="521" class="size-full wp-image-10071" />';
+  echo '</div>';
+  echo '<div class="col-sm-7">';
+  echo $section_two;
+  echo '</div>';
+  echo '</div>';
+  echo '</div>';
+  echo '</div>';
 
-    <!-- =============== Pick Your Meals =============== -->
-
-    <div class="pick-meals">
-      <div class="container">
-        <div class="row">
-            <p><img src="http://www.myprepdmeals.com/wp-content/uploads/2017/04/coconut-curry.jpg" alt="" width="768" height="521" class="alignnone size-full wp-image-10071" />After selecting your plan, signing up is quick and easy. Once you create your account, simply fill out the weekly meal and snack selection form to choose your food for the week. Then, get ready for your first fresh delivery early Monday morning!</p>
-
-<p>Our goal is to be as flexible as you are, so we give you as much choice as possible. Each week, you have the option to hand-pick your meals and snacks from our constantly-rotating menu OR let our chefs surprise you with their selections. Simply make your picks before Thursday night at midnight.</p>
-            <div class="middle-img">
-            <a href="<?php echo site_url(); ?>/meal-plans/" class="btn btn-plan">Make Your Picks</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <!-- =============== Pick Your Meals =============== -->
-
-    <!-- =============== Have a Powerful Week =============== -->
-     <div class="pick-plan parallax-3">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-12 wow fadeIn">
-            <h2 class="text-title-2">Have a Powerful Week</h2>
-            <p>The first half of your delivery will arrive between 4 and 7 a.m. Monday morning, and the second half will arrive Thursday morning. Simply leave your empty bag and ice packs outside the night before delivery, and you’ll wake up to delicious, nutritious food to start your week off on the right foot!</p>
-
-<p>We get it – you’re busy. And we want you to stay busy with the things you love, without having to sacrifice the type of nutrition that’s going to help you do them. With Prep’d, you can cross “meal prep” off your to-do list, and then conquer the rest of that list at your highest possible level.</p>
-
-<p>No matter how you fill your busy schedule, Prep’d will fill your fridge with the food you need to fuel it. <a class="bl-link" href="http://www.myprepdmeals.com/meal-plans/">Get started</a> today, and tackle your week like the warrior you are.</p>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- =============== Have a Powerful Week =============== -->
-<?php get_footer('plans'); ?>
+  // content section three
+  echo '<div class="pick-plan parallax-3" style="background: url(\'' . $background_image . '\')">';
+  echo '<div class="container">';
+  echo '<div class="row">';
+  echo '<div class="col-sm-12 wow fadeIn">';
+  echo '<h2 class="text-title-2">Have a Powerful Week</h2>';
+  echo '<p>The first half of your delivery will arrive between 4 and 7 a.m. Monday morning, and the second half will arrive Thursday morning. Simply leave your empty bag and ice packs outside the night before delivery, and you’ll wake up to delicious, nutritious food to start your week off on the right foot!</p>';
+  echo '<p>We get it – you’re busy. And we want you to stay busy with the things you love, without having to sacrifice the type of nutrition that’s going to help you do them. With Prep’d, you can cross “meal prep” off your to-do list, and then conquer the rest of that list at your highest possible level.</p>';
+  echo '<p>No matter how you fill your busy schedule, Prep’d will fill your fridge with the food you need to fuel it. <a class="bl-link" href="http://www.myprepdmeals.com/meal-plans/">Get started</a> today, and tackle your week like the warrior you are.</p>';
+  echo '</div>';
+  echo '</div>';
+  echo '</div>';
+  echo '</div>';
+     
+get_footer('plans'); ?>
